219f1d1a68bf27c3f771a0907b929085bef9203c
Lucas <lucasjlima2006@gmail.com>
2025-10-20 12:22:33 -0300

    adding News entity

---
+++ Models/News.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace News_Manager.Models;
+
+public class News
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(150)]
+    public string Title { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string Author { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime PublishDate { get; set; }
+
+    [Required]
+    public Category Category { get; set; }
+
+    [Required]
+    [DataType(DataType.MultilineText)]
+    public string Content { get; set; }
+
+    public bool IsPublished { get; set; }
+}
+}
