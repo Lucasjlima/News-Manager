1aed1fa47f01dee4bf1e15e6920776ae16955d6d
Lucas <lucasjlima2006@gmail.com>
2025-10-20 12:07:05 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace News_Manager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
