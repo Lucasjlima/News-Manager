9fea3a3f9193fcc638aae6853bd8ee435bac50c3
Lucas <lucasjlima2006@gmail.com>
2025-10-20 12:09:34 -0300

    adding readme

--- obj/Debug/net8.0/News-Manager.AssemblyInfo.cs
+++ obj/Debug/net8.0/News-Manager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("News-Manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aed1fa47f01dee4bf1e15e6920776ae16955d6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("News-Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("News-Manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
